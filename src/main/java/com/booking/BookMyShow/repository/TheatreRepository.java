881b31e3268f426e78137577be38c3bbc177ec5d
98001yash <yashchauhan.gaya@gmail.com>
2026-02-28 13:28:13 +0530

    added the theatre repository

---
+++ src/main/java/com/booking/BookMyShow/repository/TheatreRepository.java
@@ -0,0 +1,36 @@
+package com.booking.BookMyShow.repository;
+
+import com.booking.BookMyShow.entity.Theatre;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TheatreRepository extends JpaRepository<Theatre,Long> {
+
+    // Duplicate check (same name inside city
+    boolean existsByNameIgnoreCaseAndCity_Id(
+            String name,
+            Long cityId
+    );
+
+    // Slug lookup (for routing)
+    Optional<Theatre> findBySlugAndActiveTrue(String slug);
+
+    // City based filtering (ADMIN)
+    Page<Theatre> findAllByCity_Id(
+            Long cityId,
+            Pageable pageable
+    );
+
+    // Public city-based filtering
+    Page<Theatre> findAllByCity_IdAndActiveTrue(
+            Long cityId,
+            Pageable pageable
+    );
+
+    // Simple list for internal use
+    List<Theatre> findAllByCity_IdAndActiveTrue(Long cityId);
+}
