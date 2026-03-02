c16efeb78e0dcd22bd4dc2a8a7f599200573462a
98001yash <yashchauhan.gaya@gmail.com>
2026-03-02 10:49:31 +0530

    updated the screen entity and added the screeType enum

--- src/main/java/com/booking/BookMyShow/entity/Screen.java
+++ src/main/java/com/booking/BookMyShow/entity/Screen.java
@@ -1,8 +1,11 @@
 package com.booking.BookMyShow.entity;
 
 
+import com.booking.BookMyShow.enums.ScreenType;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "screens",
@@ -24,9 +27,26 @@ public class Screen {
     private String name;
 
     @Column(nullable = false)
+    private String slug;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ScreenType screenType;
+
+    @Column(nullable = false)
     private Integer totalSeats;
+
+
+    @Column(nullable = false)
+    private Boolean isActive;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "theatre_id", nullable = false)
     private Theatre theatre;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
 }
---
+++ src/main/java/com/booking/BookMyShow/enums/ScreenType.java
@@ -0,0 +1,9 @@
+package com.booking.BookMyShow.enums;
+
+public enum ScreenType {
+
+    TWO_D,
+    THREE_B,
+    IMAX,
+    FOUR_DX
+}
