bb5b8e0c53e614c4868ade2f8504a37ed822343e
98001yash <yashchauhan.gaya@gmail.com>
2026-03-02 15:41:07 +0530

    now the screen and seats module are working fine

--- src/main/java/com/booking/BookMyShow/repository/ShowSeatInventoryRepository.java
+++ src/main/java/com/booking/BookMyShow/repository/ShowSeatInventoryRepository.java
@@ -13,28 +13,24 @@ import java.util.Optional;
 public interface ShowSeatInventoryRepository
         extends JpaRepository<ShowSeatInventory, Long> {
 
-    // 🔹 Find single seat by show + seat number (via SeatLayout relation)
+
     Optional<ShowSeatInventory> findByShowIdAndSeatLayout_SeatNumber(
             Long showId,
             String seatNumber
     );
 
-    // 🔹 Fetch seats by show + status (AVAILABLE / LOCKED / BOOKED)
     List<ShowSeatInventory> findByShowIdAndStatus(
             Long showId,
             SeatStatus status
     );
 
-    // 🔹 Count seats by status (useful for analytics / availability)
     long countByShowIdAndStatus(Long showId, SeatStatus status);
 
-    // 🔹 Find expired locks
     List<ShowSeatInventory> findByStatusAndLockedUntilBefore(
             SeatStatus status,
             LocalDateTime time
     );
 
-    // 🔹 Pessimistic lock version (for critical seat locking)
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<ShowSeatInventory> findWithLockByShowIdAndSeatLayout_SeatNumber(
             Long showId,
