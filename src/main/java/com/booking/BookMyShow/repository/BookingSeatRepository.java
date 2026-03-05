0c68ce85266dce26d576f759eeb1d622b98edf0a
98001yash <yashchauhan.gaya@gmail.com>
2026-03-05 12:49:00 +0530

    updated the booking and bookingSeatReposiiry

--- src/main/java/com/booking/BookMyShow/repository/BookingSeatRepository.java
+++ src/main/java/com/booking/BookMyShow/repository/BookingSeatRepository.java
@@ -1,11 +1,13 @@
 package com.booking.BookMyShow.repository;
 
+import com.booking.BookMyShow.entity.Booking;
 import com.booking.BookMyShow.entity.BookingSeat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface BookingSeatRepository extends JpaRepository<BookingSeat, Long> {
+public interface BookingSeatRepository
+        extends JpaRepository<BookingSeat, Long> {
 
-    List<BookingSeat> findByBookingId(Long bookingId);
+    List<BookingSeat> findByBooking(Booking booking);
 }
