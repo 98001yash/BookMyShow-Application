0639c3c3728fa5239cd689c6a2e65469fb95fff9
98001yash <yashchauhan.gaya@gmail.com>
2026-03-06 18:07:00 +0530

    EmailService created

---
+++ src/main/java/com/booking/BookMyShow/service/EmailService.java
@@ -0,0 +1,35 @@
+package com.booking.BookMyShow.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+
+    private final JavaMailSender mailSender;
+
+    public void sendBookingConfirmation(String toEmail,
+                                        String bookingReference,
+                                        String movieName,
+                                        String showTime){
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setTo(toEmail);
+        message.setSubject("🎬 Booking Confirmed - BookMyShow");
+
+        message.setText(
+                "Your booking is confirmed!\n\n" +
+                        "Booking Reference: " + bookingReference + "\n" +
+                        "Movie: " + movieName + "\n" +
+                        "Show Time: " + showTime + "\n\n" +
+                        "Enjoy your movie! 🍿"
+        );
+
+        mailSender.send(message);
+    }
+}
